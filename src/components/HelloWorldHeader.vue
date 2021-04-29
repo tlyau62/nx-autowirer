43bac0b56972963e7b021de76258d32eb79db800
Tsz Lam <tyautl@nexify.com.hk>
2021-04-29 10:07:25 +0800

    Add basic

---
+++ src/components/HelloWorldHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button @click="$emit('emit')">emit</button>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <HelloWorld>
+      <template #header>
+        <HelloWorldHeader v-autowired>
+          Test
+        </HelloWorldHeader>
+      </template>
+    </HelloWorld>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorldHeader from "@/components/HelloWorldHeader.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    HelloWorldHeader,
+  },
+};
 </script>
